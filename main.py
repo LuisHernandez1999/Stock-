c1db856eb80a798273128d3fd471df15908e4605
LuisHernandez1999 <luisohernandez27@gmail.com>
2024-12-14 22:26:48 -0300

    adicoes visuais

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ app_logo.place(x=0, y=0)
 ### frame meio (entradas)
 l_nome= Label(frameMeio, text="Nome", height=1, anchor=NW, font=('Ivy 10 bold'), bg= co1,fg =co4)
 l_nome.place(x= 10, y=10)
-entrada_nome= Entry(frameMeio, width=25, justify='left',relief=SOLID)
+entrada_nome= Entry(frameMeio, width=30, justify='left',relief=SOLID)
 entrada_nome.place(x= 130, y= 11)
 
 l_descricao= Label(frameMeio, text="Descrição", height=1, anchor=NW, font=('Ivy 10 bold'), bg= co1,fg =co4)
@@ -57,7 +57,7 @@ entrada_descricao.place(x= 130, y= 41)
 
 l_marca= Label(frameMeio, text="Marca", height=1, anchor=NW, font=('Ivy 10 bold'), bg= co1,fg =co4)
 l_marca.place(x= 10, y=70)
-entrada_marca= Entry(frameMeio, width=25, justify='left',relief=SOLID)
+entrada_marca= Entry(frameMeio, width=30, justify='left',relief=SOLID)
 entrada_marca.place(x= 130, y= 72)
 
 l_data_compra= Label(frameMeio, text="Data da compra ", height=1, anchor=NW, font=('Ivy 10 bold'), bg= co1,fg =co4)
@@ -72,9 +72,113 @@ entrada_quantidade.place(x= 130, y= 133)
 
 l_serie= Label(frameMeio, text="Número de série ", height=1, anchor=NW, font=('Ivy 10 bold'), bg= co1,fg =co4)
 l_serie.place(x= 10, y=163)
-entrada_serie= Entry(frameMeio, width=25, justify='left',relief=SOLID)
+entrada_serie= Entry(frameMeio, width=30, justify='left',relief=SOLID)
 entrada_serie.place(x= 130, y= 163)
 
-l_marca=Label(frameMeio, text='Imagem do produto')
+## bs
+l_carregar=Label(frameMeio, text='Imagem do produto', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_carregar.place(x=10,y=193)
+b_carregar= Button(frameMeio,width=30,text='carregar'.upper(),compound=CENTER, anchor=CENTER, overrelief=RIDGE,font=('Ivy 8 '),bg=co1,fg=co0)
+b_carregar.place(x=140, y=191)
+
+## inserir b
+img_add = Image.open('addbtt.png')
+img_add = img_add.resize((20,20))
+img_add = ImageTk.PhotoImage(img_add)
+
+
+b_inserir= Button(frameMeio, image=img_add,width=95,text='Adicionar'.upper(),compound=LEFT, anchor=NW, overrelief=RIDGE,font=('Ivy 8 '),bg=co1,fg=co0)
+b_inserir.place(x=350, y=10)
+
+
+## update b
+img_update = Image.open('update.png')
+img_update = img_update.resize((20,20))
+img_update = ImageTk.PhotoImage(img_update)
+
+
+b_atualizar= Button(frameMeio, image=img_update,width=95,text='Atualizar'.upper(),compound=LEFT, anchor=NW, overrelief=RIDGE,font=('Ivy 8 '),bg=co1,fg=co0)
+b_atualizar.place(x=350, y=50)
+
+## delete b 
+
+img_delete = Image.open('delete.png')
+img_delete = img_delete.resize((20,20))
+img_delete = ImageTk.PhotoImage(img_delete)
+
+
+b_atualizar= Button(frameMeio, image=img_delete,width=95,text='Deletar'.upper(),compound=LEFT, anchor=NW, overrelief=RIDGE,font=('Ivy 8 '),bg=co1,fg=co0)
+b_atualizar.place(x=350, y=90)
+
+
+## b ver item 
+img_item = Image.open('item.png')
+img_item = img_item.resize((20,20))
+img_item = ImageTk.PhotoImage(img_item)
+
+
+b_item= Button(frameMeio, image=img_item,width=95,text='Ver item'.upper(),compound=LEFT, anchor=NW, overrelief=RIDGE,font=('Ivy 8 '),bg=co1,fg=co0)
+b_item.place(x=350, y=190)
+
+## labels qtnd total e valores 
+l_total= Label(frameMeio, text=" ",width=14 ,height=2, anchor=NW, font=('Ivy 17 bold'), bg= co7,fg =co1)
+l_total.place(x= 510, y=17)
+l_total= Label(frameMeio, text=" Valor total", anchor=NW, font=('Ivy 17 bold'), bg= co7,fg =co1)
+l_total.place(x= 520, y=27)
+
+l_quantidade_total= Label(frameMeio, text=" ",width=18 ,height=2, anchor=NW, font=('Ivy 17 bold'), bg= co7,fg =co1)
+l_quantidade_total.place(x= 510, y=97)
+l_quantidade_total= Label(frameMeio, text="Quantidade total", anchor=NW, font=('Ivy 17 bold'), bg= co7,fg =co1)
+l_quantidade_total.place(x= 520, y=107)
+
+
+
+## creating a treeview with dual scrollbars
+tabela_head = ['#Item','Nome', 'Descrição', 'Marca', 'Data da compra','Quantidade', 'Número de série','Fornecedor']
+
+lista_itens = []
+
+global tree
+
+tree = ttk.Treeview(frameBaixo, selectmode="extended",columns=tabela_head, show="headings")
+
+## vertical scrollbar
+vsb = ttk.Scrollbar(frameBaixo, orient="vertical", command=tree.yview)
+
+## horizontal scrollbar
+hsb = ttk.Scrollbar(frameBaixo, orient="horizontal", command=tree.xview)
+
+tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+tree.grid(column=0, row=0, sticky='nsew')
+vsb.grid(column=1, row=0, sticky='ns')
+hsb.grid(column=0, row=1, sticky='ew')
+frameBaixo.grid_rowconfigure(0, weight=12)
+
+hd=["center","center","center","center","center","center","center", 'center']
+h=[40,150,100,160,130,100,100, 100]
+n=0
+
+for col in tabela_head:
+    tree.heading(col, text=col.title(), anchor=CENTER)
+  
+    tree.column(col, width=h[n],anchor=hd[n])
+    n+=1
+
+
+##inserindo os itens dentro da tabela
+for item in lista_itens:
+    tree.insert('', 'end', values=item)
+
+
+quantidade = [8888,88]
+
+for iten in lista_itens:
+    quantidade.append(iten[6])
+
+Total_valor = sum(quantidade)
+Total_itens = len(quantidade)
+
+l_total['text'] = 'R$ {:,.2f}'.format(Total_valor)
+ ## l_qtd['text'] = Total_itens
 
 janela.mainloop()
